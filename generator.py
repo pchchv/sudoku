9f266d0cc0ccae51b0ae2f1386048f8e9a113e87
pchchv <ipchchv@gmail.com>
2022-09-28 12:56:36 +0300

    Implement a find empty func

---
+++ generator.py
@@ -0,0 +1,19 @@
+firstBoard = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+def findEmpty(board):
+    for y in range(len(board)):
+        for x in range(len(board[0])):
+            if board[y][x] == 0:
+                return y, x  # y = row , x = column
+    # if we got here it mean that we finish the sudoku, so return none
+    return None
