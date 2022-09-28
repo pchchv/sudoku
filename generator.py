cae629e99191dac0ac696c4526e491b9b7130db0
pchchv <ipchchv@gmail.com>
2022-09-28 13:04:24 +0300

    Implement a validator

--- generator.py
+++ generator.py
@@ -17,3 +17,21 @@ def findEmpty(board):
                 return y, x  # y = row , x = column
     # if we got here it mean that we finish the sudoku, so return none
     return None
+
+def validator(board, number, coordinates):
+    # checking row
+    for x in range(len(board[0])):
+        if number == board[coordinates[0]][x] and coordinates[1] != x:
+            return False
+    # checking column
+    for y in range(len(board)):
+        if number == board[y][coordinates[1]] and coordinates[0] != y:
+            return False
+    # checking the box
+    box_x = coordinates[1] // 3
+    box_y = coordinates[0] // 3
+    for y in range(box_y * 3, box_y * 3 + 3):
+        for x in range(box_x * 3, box_x * 3 + 3):
+            if number == board[y][x] and (y, x) != coordinates:
+                return False
+    return True
