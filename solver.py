24a9cedd0f24ad95c031f517ddbf336e6e4ea7f8
pchchv <ipchchv@gmail.com>
2022-09-29 11:10:06 +0300

    Implement a solver

---
+++ solver.py
@@ -0,0 +1,33 @@
+from generator import *
+import copy
+
+
+Board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+solbedBoard = copy.deepcopy(Board)
+
+
+def sudokuSolver(board):
+    # Get to the end of the board. The findEmpty function returns NONE
+    find = findEmpty(board)
+    if find is None:
+        return True
+    else:
+        row, col = find
+    for number in range(1, 10):
+        if validator(board, number, (row, col)):
+            board[row][col] = number
+            if sudokuSolver(board):
+                return True
+            board[row][col] = 0
+    return False
